d843fb0073e5a7a63f9a629333d9de1c6a1e3eff
Only_soft <abdullahwin99@gmail.com>
2020-03-04 11:30:15 +0100

    creation du composite_personnel

---
+++ src/main/java/fr/uvsq/solid/pglp_4/Composite_Personnel.java
@@ -0,0 +1,15 @@
+package fr.uvsq.solid.pglp_4;
+
+import java.util.ArrayList;
+
+public class Composite_Personnel implements Interface_Personnel {
+	private int id;
+	private ArrayList<Interface_Personnel> personnel=new ArrayList<Interface_Personnel>();
+	public void affiche()
+	{
+		
+		
+	}
+
+}
+
--- src/main/java/fr/uvsq/solid/pglp_4/Personnels.java
+++ src/main/java/fr/uvsq/solid/pglp_4/Personnels.java
@@ -3,11 +3,17 @@ package fr.uvsq.solid.pglp_4;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
-public final class  Personnels {
- String nom;
- String prenom;
- String fonction;
- LocalDateTime date_naiss;
- ArrayList<Integer> telephone =new ArrayList<Integer>();
+public final class  Personnels implements Interface_Personnel  {
+ private int id;
+ private String nom;
+ private String prenom;
+ private String fonction;
+ private LocalDateTime date_naiss;
+ private ArrayList<Integer> telephone =new ArrayList<Integer>();
+  public void affiche()
+	{
+		System.out.println("Id: "+this.id+" Nom: "+this.nom+" Prenom: "+this.prenom);
+		
+	}
 	
 }
