067ac545f828694952006050471892d52e792823
Only_soft <abdullahwin99@gmail.com>
2020-03-04 11:20:40 +0100

    creation de l'interface  interface_personnel

---
+++ src/main/java/fr/uvsq/solid/pglp_4/Interface_Personnel.java
@@ -0,0 +1,6 @@
+package fr.uvsq.solid.pglp_4;
+
+public interface Interface_Personnel {
+
+	public void affiche();
+}
---
+++ src/main/java/fr/uvsq/solid/pglp_4/Personnels.java
@@ -0,0 +1,13 @@
+package fr.uvsq.solid.pglp_4;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public final class  Personnels {
+ String nom;
+ String prenom;
+ String fonction;
+ LocalDateTime date_naiss;
+ ArrayList<Integer> telephone =new ArrayList<Integer>();
+	
+}
