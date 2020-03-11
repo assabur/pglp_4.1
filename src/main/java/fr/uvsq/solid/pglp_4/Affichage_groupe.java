c7538ebcec46874642b4c53bfbe4a21711d5b91b
Only_soft <abdullahwin99@gmail.com>
2020-03-11 10:22:25 +0100

    modidfication de la classe groupeAffiche

--- src/main/java/fr/uvsq/solid/pglp_4/Affichage_groupe.java
+++ src/main/java/fr/uvsq/solid/pglp_4/Affichage_groupe.java
@@ -1,5 +1,35 @@
 package fr.uvsq.solid.pglp_4;
 
-public class Affichage_groupe {
+public class Affichage_groupe implements Iterator  {
+
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Interface_Personnel Next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	private class GroupeIterator implements Iterator
+	{
+
+		public boolean hasNext() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		public Interface_Personnel Next() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+		public Iterator getIterator ()
+		{
+			return new GroupeIterator();
+			
+		}
+		
+	}
 
 }
